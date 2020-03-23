cd1a87c4d730c889ce15a33e8b3a2a181197e5a1
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-23 18:09:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,10 +37,12 @@ def movies_with_director_key(name, movies_collection)
   dir_array = []
   collect_index = 0 
   while collect_index < movies_collection.length do 
-    dir_hash = {}
-    dir_hash[:director_name] = name
-    dir_hash[:title] = movies_collection[collect_index][:title]
-    dir_array.push(dir_hash)
+    binding.pry
+    
+    # dir_hash = {}
+    # dir_hash[:director_name] = name
+    # dir_hash[:title] = movies_collection[collect_index][:title]
+    # dir_array.push(dir_hash)
     collect_index += 1 
    end
    dir_array
